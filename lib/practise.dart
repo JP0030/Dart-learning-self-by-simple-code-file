f7fe20bacb4fd1ca4a2f12a16880b8b9da956ba7
JP0030 <thekeen003@gmail.com>
2024-01-31 01:53:39 +0530

    in this Flutter code I created practise.dart file in lib folder

---
+++ lib/practise.dart
@@ -0,0 +1,13 @@
+
+
+import 'dart:io';
+
+void main () {
+  print('Hello Dart !');
+  stdout.write('Enter Your Name_ :- ');
+  stdout.write('Enter Your Name_ :- ');
+  var name = stdin.readLineSync();
+  
+  stdout.write("Welcome , \"$name\"");
+
+}
